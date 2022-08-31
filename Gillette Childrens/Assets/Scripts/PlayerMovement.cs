008b9ae11ef1b6b44f39bd138715607ae8dad375
jpvogel07 <Jacob.Vogel@trojans.dsu.edu>
2022-08-31 09:02:50 -0500

    v0.002
    pick ups work (not drags)

--- Gillette Childrens/Assets/Scripts/PlayerMovement.cs
+++ Gillette Childrens/Assets/Scripts/PlayerMovement.cs
@@ -8,12 +8,13 @@ public class PlayerMovement : MonoBehaviour
 {
     private Inputs pinputs;
     private bool highlighted;
+    private GameObject grabbed;
 
     private void OnEnable()
     {
         pinputs = new Inputs();
         pinputs.Enable();
-        pinputs.player.movement.started += movment;
+        //pinputs.player.movement.started += movment;
 
     }
     private void OnDisable()
@@ -29,28 +30,46 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //this.gameObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        this.gameObject.transform.position = mousePos;
     }
 
-    private void movment(InputAction.CallbackContext c)
-    {
-        Debug.LogError("working");
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("clickable"))
         {
+            pinputs.player.movement.started += movment;
             //switch image
+            Debug.Log("highlighted");
             highlighted = true;
+            grabbed = collision.gameObject;
         } 
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("clickable"))
         {
+            pinputs.player.movement.started -= movment;
             //switch image
             highlighted = false;
+            grabbed = null;
         }
     }
+    private void movment(InputAction.CallbackContext c)
+    {
+        Debug.Log("click");
+        if (highlighted)
+        {
+            grabbed.gameObject.transform.parent = this.transform;
+            highlighted = false;
+        }
+        else if(!highlighted&&grabbed.gameObject.transform.parent!=null)
+        {
+            grabbed.gameObject.transform.parent = null;
+        }
+    }
+
 }
