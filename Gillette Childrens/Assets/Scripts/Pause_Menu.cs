c98177d9731ecfe2f1b12b3265ac46cedcd0854e
Howi7 <71466823+Howi7@users.noreply.github.com>
2022-11-02 10:43:26 -0500

    Pause Menu Test
    Implemented a Pause Menu in the test scene

---
+++ Gillette Childrens/Assets/Scripts/Pause_Menu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject pauseButton;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        pauseButton.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        pauseButton.SetActive(true);
+        Time.timeScale = 1f;
+    }
+
+    public void mainMenu()
+    {
+        Debug.LogError("No functioning main menu yet");
+    }
+
+    public void close()
+    {
+        Application.Quit();
+    }
+}
