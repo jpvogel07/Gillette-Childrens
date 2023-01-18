4c1efcf32ce72c46b56b240ed7a2d8012a890892
Howi7 <71466823+Howi7@users.noreply.github.com>
2023-01-18 09:10:51 -0600

    Began testing for inventory system

--- Gillette Childrens/Assets/Scripts/Backpack_Inventory.cs
+++ Gillette Childrens/Assets/Scripts/Backpack_Inventory.cs
@@ -1,11 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Backpack_Inventory : MonoBehaviour
 {
     [SerializeField] GameObject Menu;
+    [SerializeField] Image[] initialized;
+    [SerializeField] Sprite[] Silhouettes;
+    [SerializeField] Sprite[] Images;
+    public bool[] obtained;
 
+    private void Start()
+    {
+        int k = 0;
+        foreach (Image item in initialized)
+        {
+            initialized[k].sprite = Silhouettes[k];
+            obtained[k] = false;
+            k++;
+        }
+    }
+
+    public void obtainAll()
+    {
+        int k = 0;
+        foreach (Image item in initialized)
+        {
+            if (obtained[k] == false)
+            {
+                initialized[k].sprite = Images[k];
+                obtained[k] = true;
+            } else
+            {
+                initialized[k].sprite = Silhouettes[k];
+                obtained[k] = false;
+            }
+            k++;
+        }
+
+    }
     public void openInventory()
     {
         Menu.SetActive(true);
@@ -15,4 +49,6 @@ public class Backpack_Inventory : MonoBehaviour
     {
         Menu.SetActive(false);
     }
+
+
 }
