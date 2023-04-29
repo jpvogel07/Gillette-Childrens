c9e54a01a4b15ccd161caab41d2bfac81283c851
Argo44239 <97545289+Argo44239@users.noreply.github.com>
2023-04-29 14:41:08 -0500

    credits complete

--- Gillette Childrens/Assets/Scripts/Fade.cs
+++ Gillette Childrens/Assets/Scripts/Fade.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Fade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float fadeSpeed=1f;
+    public Image img;
+
+    void OnEnable()
     {
-        
+        StartCoroutine(FadeImage());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator FadeImage()
     {
-        
+        for (float i = fadeSpeed; i >= 0; i -= Time.deltaTime)
+        {
+            img.color = new Color(255, 255, 255, i);
+            yield return null;
+        }
     }
 }
